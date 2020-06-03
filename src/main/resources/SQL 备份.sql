e433bf3fda2cf55365c575f4f6b5944f109e0857
e <1379897615@qq.com>
2020-06-03 18:35:44 +0800

    intellij 修改 2020-06-03 18:35

--- src/main/resources/SQL 备份.sql
+++ src/main/resources/SQL 备份.sql
@@ -4,27 +4,24 @@ create database mybatis_Test;
 
 -- 创建测试表
 drop table if exists user;
-create table user(
-id int(11) not null auto_increment,
-username VARCHAR(32) not null COMMENT '用户名称',
-birthday datetime default null COMMENT '生日',
-sex char(1) default null COMMENT'性别',
-address varchar(256) default null COMMENT '地址',
-primary key (id)
-)ENGINE = INNODB default charset = utf8;
+create table user
+(
+    id       int(11)     not null auto_increment,
+    username VARCHAR(32) not null COMMENT '用户名称',
+    birthday datetime     default null COMMENT '生日',
+    sex      char(1)      default null COMMENT '性别',
+    address  varchar(256) default null COMMENT '地址',
+    primary key (id)
+) ENGINE = INNODB
+  default charset = utf8;
 
 -- 追加数据
 
-insert into user(username,birthday,sex,address) values('张三','2000-01-03 19:13:12','男','湖北武汉'),('晨晨','1996-01-03 19:13:12','女','湖北武汉');
+insert into user(username, birthday, sex, address)
+values ('张三', '2000-01-03 19:13:12', '男', '湖北武汉'),
+       ('晨晨', '1996-01-03 19:13:12', '女', '湖北武汉');
 
-select * from user
+select *
+from user
+where id > 2;
 
--- github 修改
--- github 修改2 github 修改3  github 修改4
-
-
--- interllij 修改测试
--- interllij 修改测试2
--- interllij 修改测试3
-
--- interllij 修改测试4
