521f4fffd61780cfc8dccc87717729b06a0b2e2d
e <1379897615@qq.com>
2020-06-03 22:04:47 +0800

    intellij 修改 2020-06-03 22:04

---
+++ target/classes/SQL 备份.sql
@@ -0,0 +1,25 @@
+-- 创建测试数据库 mybatis_Test
+create database mybatis_Test;
+
+
+-- 创建测试表
+drop table if exists user;
+create table user
+(
+    id       int(11)     not null auto_increment,
+    username VARCHAR(32) not null COMMENT '用户名称',
+    birthday datetime     default null COMMENT '生日',
+    sex      char(1)      default null COMMENT '性别',
+    address  varchar(256) default null COMMENT '地址',
+    primary key (id)
+) ENGINE = INNODB
+  default charset = utf8;
+
+-- 追加数据
+
+insert into user(username, birthday, sex, address)
+values ('张三', '2000-01-03 19:13:12', '男', '湖北武汉'),
+       ('晨晨', '1996-01-03 19:13:12', '女', '湖北武汉'),
+       ('何妮妮', '1998-12-15 14:25:45', '女', '湖北十堰'),
+       ('万和', '1988-02-15 01:12:12', '男', '湖北黄石');
+
