d3510fd14f45bd1ed3585166d9effada4a74d0fe
e <1379897615@qq.com>
2020-06-04 16:01:38 +0800

    intellij 修改分支1 2020-06-04 16:01

--- src/test/java/com/MybatisTest.java
+++ src/test/java/com/MybatisTest.java
@@ -2,6 +2,7 @@ package com;
 
 
 import com.dao.IUserDao;
+import com.dao.impl.IUserDaoImpl;
 import com.domain.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -22,23 +23,44 @@ public class MybatisTest {
     /*
      *  mybatis 入门案例，测试
      * */
+//
+//    @Test
+//    public void test() throws IOException {
+//        //1.读取xml配置文件
+//        InputStream is = Resources.getResourceAsStream("SqlMapConfig.xml");
+//        //2.创建 SqlSessionFactory 工厂
+//        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+//        SqlSessionFactory factory = builder.build(is); // SqlSessionFactory是接口不能直接使用
+//        //3.使用工厂生产SqlSession对象
+//        SqlSession sqlSession = factory.openSession();
+//        //4.使用 sqlSession 创建 Dao 接口的代理对象
+//        IUserDao mapper = sqlSession.getMapper(IUserDao.class);
+//        //5.使用代理对象执行方法
+//        List<User> all = mapper.findAll();
+//        for (User user : all)
+//            System.out.println(user.toString());
+//        sqlSession.close();
+//        is.close();
+//
+//    }
+
+    /*
+     *  测试到实现类
+     * */
 
     @Test
-    public void test() throws IOException {
+    public void testDaoImpl() throws IOException {
         //1.读取xml配置文件
         InputStream is = Resources.getResourceAsStream("SqlMapConfig.xml");
         //2.创建 SqlSessionFactory 工厂
         SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
         SqlSessionFactory factory = builder.build(is); // SqlSessionFactory是接口不能直接使用
-        //3.使用工厂生产SqlSession对象
-        SqlSession sqlSession = factory.openSession();
-        //4.使用 sqlSession 创建 Dao 接口的代理对象
-        IUserDao mapper = sqlSession.getMapper(IUserDao.class);
-        //5.使用代理对象执行方法
-        List<User> all = mapper.findAll();
+        //3.使用工厂创建dao对象
+        IUserDaoImpl iUserDao = new IUserDaoImpl(factory);
+        //4.使用代理对象执行方法
+        List<User> all = iUserDao.findAll();
         for (User user : all)
             System.out.println(user.toString());
-        sqlSession.close();
         is.close();
 
     }
